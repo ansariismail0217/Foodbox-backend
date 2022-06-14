c35e9942a8976ba5e08746fb601bab7244782d71
Abdullah Ismail Ansari <ansariismail0217@gmail.com>
2022-06-14 20:34:05 +0530

    release-02: search-filters

--- src/main/java/com/foodboxapp/dao/ProductRepository.java
+++ src/main/java/com/foodboxapp/dao/ProductRepository.java
@@ -1,12 +1,19 @@
 package com.foodboxapp.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.foodboxapp.entity.Product;
 
 // Entity= Product, Primary Key= Long
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+	List<Product> findByCategoryId(@RequestParam("id") Long id);
+	
+	List<Product> findByNameContaining(@RequestParam("name") String name);
 
 }
